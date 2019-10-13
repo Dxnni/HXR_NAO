6e68fb61ad63d9c2424f1264b0300d753721d929
Daniel Bramwell <dbramwell@ufl.edu>
2019-10-13 16:09:54 -0400

    converted npm scripts for windows

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
+// const {PythonShell} = require('python-shell')
+// const {ipcMain} = require('electron');
 
 const { app } = electron;
 const { BrowserWindow } = electron;
@@ -38,3 +40,25 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+/*
+// Attach listener in the main process with the given ID
+ipcMain.on('request-mainprocess-action', (event, arg) => {
+  // Displays the object sent from the renderer process:
+  //{
+  //    message: "Hi",
+  //    someData: "Let's go"
+  //}
+  console.log(
+      arg
+  );
+  console.log('TESSSSSSSSSSSSSSSSSSSSSSSSSSSST');
+
+});
+  // let py_string = 'from naoqi import ALProxy;tts=ALProxy("ALTextToSpeech", "10.0.1.133", 9559);tts.say("Hi Jean!")';
+
+  // PythonShell.runString(py_string, null, function (err) {
+  //   if (err) throw err;
+  //   console.log('finished');
+  // });
+*/
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import {PythonShell} from 'python-shell';
+// const { ipcRenderer } = require('electron');
 
 function App() {
   return (
@@ -24,3 +26,28 @@ function App() {
 }
 
 export default App;
+
+/*
+// let script_path = 'C:/Users/Bramw/Desktop/Fall19/Research/code/electron-react-boilerplate-1/scripts/pythonNAO.py';
+  // PythonShell.run(script_path, null, function (err) {
+  //   if (err) throw err;
+  //   console.log('finished');
+  // });
+  // let py_string = 'from naoqi import ALProxy;tts=ALProxy("ALTextToSpeech", "10.0.1.133", 9559);tts.say("Hi Jean!")';
+
+  // PythonShell.runString(py_string, null, function (err) {
+  //   if (err) throw err;
+  //   console.log('finished');
+  // });
+
+  // Some data that will be sent to the main process
+  let Data = {
+      message: "Hi",
+      someData: "Let's go"
+  };
+
+  // Send information to the main process
+  // if a listener has been set, then the main process
+  // will react to the request !
+  ipcRenderer.send('request-mainprocess-action', Data);
+  */
