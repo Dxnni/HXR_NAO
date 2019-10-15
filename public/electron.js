c7626a0a241545efed77a81e3dd1db5cb42325e8
Daniel Bramwell <dbramwell@ufl.edu>
2019-10-15 14:08:46 -0400

    Fixed python error by specifying which python to use within the script

--- public/electron.js
+++ public/electron.js
@@ -38,19 +38,39 @@ function createWindow() {
 ipcMain.on('request-mainprocess-speak', (event) => {
   console.log('Main received request to speak from renderer');
 
+  /*
+  //can set options for pythonShell
+  let options = {
+    mode: 'text',
+    pythonPath: 'C:/Users/Bramw/AppData/Local/Programs/Python/',
+    pythonOptions: ['-u'], // get print results in real-time
+    scriptPath: 'C:/Users/Bramw/Desktop/Fall19/Research/code/pythonScript/',
+    args: ['value1', 'value2', 'value3']
+  };
+
+  //can run python code from a string
+  let py_string = 'from naoqi import ALProxy;tts=ALProxy("ALTextToSpeech", "10.0.1.133", 9559);tts.say("Hi Jean!")';
+  PythonShell.runString(py_string, null, function (err, output) {
+    if (err) throw err;
+    console.log('output of python code:', output);
+  });
+
+  //can set specific python path to use
+  PythonShell.defaultPythonPath = 'C:/Users/Bramw/AppData/Local/Programs/Python/';
+
+  //can show the current python path and version
+  console.log('default electron python path:',PythonShell.getPythonPath());
+  PythonShell.getVersion().then((output)=>{
+    console.log('default electron python version:',output.stdout)
+    console.log('default electron python version error:',output.stderr)
+  })
+  */
+ 
   let script_path = 'C:/Users/Bramw/Desktop/Fall19/Research/code/pythonScript/pythonNAO.py';
   PythonShell.run(script_path, null, function (err, output) {
     if (err) throw err;
-    console.log('finished running python script:', output);
+    console.log('output of python script:', output);
   });
-
-  // // let py_string = 'from naoqi import ALProxy;tts=ALProxy("ALTextToSpeech", "10.0.1.133", 9559);tts.say("Hi Jean!")';
-  // let py_string = 'print("Hello Python World");print("Hello Twice!");';
-
-  // PythonShell.runString(py_string, null, function (err, output) {
-  //   if (err) throw err;
-  //   console.log('finished running python code:', output);
-  // });
 });
 
 app.on('ready', createWindow);
